a4058cbe50d583694edfcefaf69c14b7863898c4
ElasticSea <info@elasticsea.com>
2020-01-05 12:41:35 +0100

    Adding simple player controller.

---
+++ Assets/_Project/Scripts/SimplePlayerController.cs
@@ -0,0 +1,14 @@
+﻿using _Framework.Scripts.Extensions;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class SimplePlayerController : MonoBehaviour
+    {
+        [SerializeField] private float speed = 2;
+        private void Update()
+        {
+            transform.position += Time.deltaTime * speed * OVRInput.Get(OVRInput.RawAxis2D.LThumbstick).ToXZ();
+        }
+    }
+}
