144f5a1265237dbc4dc15158a93443d820b58b81
ElasticSea <info@elasticsea.com>
2020-01-18 23:34:25 +0100

    Adding more block types.

--- Assets/_Project/Scripts/BlockGrab.cs
+++ Assets/_Project/Scripts/BlockGrab.cs
@@ -2,31 +2,32 @@ using UnityEngine;
 
 namespace _Project.Scripts
 {
-    public class BlockGrab : OVRGrabbable
+    public class BlockGrab : MonoBehaviour
     {
+        [SerializeField] private Grabbable grabbable;
         [SerializeField] private Block block;
+
+        private void Awake()
+        {
+            grabbable.OnGrabBegin += delegate((OVRGrabber hand, Collider grabPoint) tuple)
+            {
+                block.BeginSnap();
+            };
+            
+            grabbable.OnGrabEnd += delegate((Vector3 linearVelocity, Vector3 angularVelocity) tuple)
+            {
+                block.EndSnap();
+            };
+        }
 
         public Block Block
         {
-            get => block;
             set => block = value;
-        }
-
-        public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
-        {
-            base.GrabBegin(hand, grabPoint);
-            block.BeginSnap();
-        }
-
-        public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
-        {
-            base.GrabEnd(linearVelocity, angularVelocity);
-            block.EndSnap();
         }
 
         public void SetupGrabPoints(Collider[] colliders)
         {
-            m_grabPoints = colliders;
+            grabbable.SetupGrabPoints(colliders);
         }
     }
 }
---
+++ Assets/_Project/Scripts/Grabbable.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+namespace _Project.Scripts
+{
+    public class Grabbable : OVRGrabbable
+    {
+        public event Action<(OVRGrabber hand, Collider grabPoint)> OnGrabBegin = tuple => { };
+        public event Action<(Vector3 linearVelocity, Vector3 angularVelocity)> OnGrabEnd = tuple => { };
+        public override void GrabBegin(OVRGrabber hand, Collider grabPoint)
+        {
+            base.GrabBegin(hand, grabPoint);
+            OnGrabBegin((hand, grabPoint));
+        }
+
+        public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+        {
+            base.GrabEnd(linearVelocity, angularVelocity);
+            OnGrabEnd((linearVelocity, angularVelocity));
+        }
+
+        public void SetupGrabPoints(Collider[] colliders)
+        {
+            m_grabPoints = colliders;
+        }
+    }
+}
